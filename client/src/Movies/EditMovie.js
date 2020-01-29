41bd993fed95c4f4f81841e6c0786e7424128190
Nolan Picini <nolanpic@gmail.com>
2020-01-29 13:22:24 -0800

    Rough in edit form

--- client/src/Movies/EditMovie.js
+++ client/src/Movies/EditMovie.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EditMovie = props => {
+
+    const initialState = {
+        title: '',
+        director: '',
+        metascore: 0,
+        stars: []
+    };
+
+    const [movie, setMovie] = useState(initialState);
+
+    const handleChange = e => {
+        const newValue = e.target.value;
+
+        if(e.target.name === 'stars') {
+            newValue = e.target.value.split(',');
+        }
+
+        setMovie({
+            ...movie,
+            [e.target.name]: newValue
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+
     return (
-        <form>
-            <p>Edit</p>
-        </form>
+        <div className="card">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Title</p>
+                    <input
+                        type="text"
+                        name="title"
+                        value={movie.title}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    <p>Director</p>
+                    <input
+                        type="text"
+                        name="director"
+                        value={movie.director}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    <p>Metascore</p>
+                    <input
+                        type="number"
+                        name="metascore"
+                        value={movie.metascore}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    <p>Stars</p>
+                    <input
+                        type="text"
+                        name="stars"
+                        value={movie.stars.join(', ')}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button style={{display: 'block',  marginTop: '2rem'}} type="Submit">Save</button>
+            </form>
+        </div>
     );
 };
 
